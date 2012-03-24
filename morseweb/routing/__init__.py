7d3a5fda317642bc14572294cc3d5c733bd05d65
Matt Jeffery <matt@clan.se>
2012-03-24 21:22:44 +0100

    renamed home view to user

--- morseweb/routing/__init__.py
+++ morseweb/routing/__init__.py
@@ -2,7 +2,7 @@
 
 def add_routes(config, **settings):
     """Add routing rules"""
-    config.add_route('home', '/')
+    config.add_route('user', '/user')
     
     config.add_route('soundcloud_callback', '/soundcloud/oauth-callback')
     config.add_route('soundcloud_connect', '/soundcloud/connect')
