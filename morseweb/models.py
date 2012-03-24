9bba10b8cae67b5f99e10d5f472df6d87c8a7826
Matt Jeffery <matt@clan.se>
2012-03-24 15:20:08 +0100

    fix for mysql using oursql

--- morseweb/models.py
+++ morseweb/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     Integer,
     Text,
+    Unicode,
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -19,7 +20,7 @@ Base = declarative_base()
 class MyModel(Base):
     __tablename__ = 'models'
     id = Column(Integer, primary_key=True)
-    name = Column(Text, unique=True)
+    name = Column(Unicode(25), unique=True)
     value = Column(Integer)
 
     def __init__(self, name, value):
