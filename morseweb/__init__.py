658dbbe8815d7372740b2fca78966f65ebbd3b2c
Matt Jeffery <matt@clan.se>
2012-03-24 14:14:19 +0100

    load envvars for config options

--- morseweb/__init__.py
+++ morseweb/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
@@ -8,6 +10,12 @@ def main(global_config, **settings):
     """
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
+
+    # load some config options from the env vars
+    for option, value in settings.items():
+        if value.startswith('$'):
+            settings[option] = os.environ.get(value[1:])
+
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
