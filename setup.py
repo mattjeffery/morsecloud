a06d215444c386bbe6ffd3eb61317118de706887
Matt Jeffery <matt@clan.se>
2012-03-24 14:24:41 +0100

    removed long description

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
-CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
     'pyramid',
@@ -19,7 +17,6 @@ requires = [
 setup(name='morseweb',
       version='0.0',
       description='morseweb',
-      long_description=README + '\n\n' +  CHANGES,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pylons",
