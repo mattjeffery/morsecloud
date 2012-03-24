e1cdb88648b0a3b51dc4459d4927086af408c34e
Matt Jeffery <matt@clan.se>
2012-03-24 12:16:02 +0100

    4 spaces instead of 3 ...

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from pyramid.config import Configurator
 from pyramid.response import Response
 
 def hello_world(request):
-   return Response('Hello %(name)s!' % request.matchdict)
+    return Response('Hello %(name)s!' % request.matchdict)
 
 if __name__ == '__main__':
     import os
 
-   config = Configurator()
-   config.add_route('hello', '/hello/{name}')
-   config.add_view(hello_world, route_name='hello')
-   app = config.make_wsgi_app()
-   port = int(os.environ.get('PORT', 8080))
-   server = make_server('0.0.0.0', port, app)
-   server.serve_forever()
+    config = Configurator()
+    config.add_route('hello', '/hello/{name}')
+    config.add_view(hello_world, route_name='hello')
+    app = config.make_wsgi_app()
+    port = int(os.environ.get('PORT', 8080))
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()
