3175a570f9e4bf7d1e611f5ad12329fafd0ce969
Matt Jeffery <matt@clan.se>
2012-03-24 20:03:28 +0100

    added home page

--- morseweb/routing/__init__.py
+++ morseweb/routing/__init__.py
@@ -2,6 +2,8 @@
 
 def add_routes(config, **settings):
     """Add routing rules"""
+    config.add_route('home', '/')
+    
     config.add_route('soundcloud_callback', '/soundcloud/oauth-callback')
     config.add_route('soundcloud_connect', '/soundcloud/connect')
 
